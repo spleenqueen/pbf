ead359534cb5e8c89cf21172105e8423730eaa33
a.hover <a.hover@ctidigital.com>
2019-08-18 18:42:42 +0100

    Moved ciders and perries to tabs again and fixed list searchers.

--- js/main.js
+++ js/main.js
@@ -8,18 +8,40 @@ window.onload = () => {
 };
 
 $(function () {
-    if ($('#beers').length) {
-        var beerOptions = {
-            valueNames: ['brewery', 'name', 'abv', 'bar', 'region']
-        };
-        var beerList = new List('beers', beerOptions);
-    }
+    // if ($('#beers').length) {
+    //     var beerOptions = {
+    //         valueNames: ['brewery', 'name', 'abv', 'bar', 'region']
+    //     };
+    //     var beerList = new List('beers', beerOptions);
+    // }
+    //
+    // if ($('#ciders').length) {
+    //     var ciderOptions = {
+    //         valueNames: ['type', 'mill', 'name', 'abv', 'region']
+    //     };
+    //     var ciderList = new List('ciders', ciderOptions);
+    // }
 
-    if ($('#ciders').length) {
-        var ciderOptions = {
-            valueNames: ['type', 'mill', 'name', 'abv', 'region']
-        };
-        var ciderList = new List('ciders', ciderOptions);
+    var $filterableTables = $('[data-filterable]');
+    if ($filterableTables.length) {
+        $filterableTables.each(function () {
+            var $table = $(this);
+            var tableId = $table.data('filterable');
+            var $sortableHeaders = $table.find('thead [data-sort]');
+            var valueNames = [];
+            $sortableHeaders.each(function() {
+                valueNames.push($(this).data('sort'));
+            });
+
+            var filterOptions = {
+                valueNames: valueNames
+            };
+
+            console.log(filterOptions);
+            console.log(tableId);
+
+            var list = new List(tableId, filterOptions);
+        });
     }
 
     // Add tooltips.
